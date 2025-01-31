b3aca174eefb287e3876c7d0eaccd6eadd5ce18c
Youness Amzil <amzilyouness@gmail.com>
2025-01-31 23:05:41 +0100

    Starting off

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import { useEffect, useState } from "react"
-
-const Card = ({ title }) => {
-    const [count, setCount] = useState(0);
-    const [hasLiked, setHasLiked] = useState(false);
-
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-    }, [hasLiked]);
-
-    return (
-        <div className="card" onClick={() => setCount(count + 1)}>
-            <h2>{title} <br/> {count || null} </h2>
-
-            <button onClick={() => setHasLiked(!hasLiked)}>
-                {hasLiked ? "❤️" : "🤍"}
-            </button>
-        </div>
-    )
-}
+import React from 'react'
 
 const App = () => {
-
     return (
-        <div className="card-container">
-            <Card title="Stars Wars" rating={5} isCool={true}/>
-            <Card title="Avatar"/>
-            <Card title="The Lion King"/>
-        </div>
+        <main>
+            <div className='pattern' />
+            <div className='wrapper'>
+                <header>
+                    <img src="./hero.png" alt="Hero Banner" />
+                    <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+                <p>Search</p>
+            </div>
+        </main>
     )
 }
 
