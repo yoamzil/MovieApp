833dd2160271c9e45cd751ab13f5acd29ca29ab4
Youness Amzil <amzilyouness@gmail.com>
2025-01-31 23:26:40 +0100

    Done with the heading part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <main>
             <div className='pattern' />
@@ -9,7 +12,7 @@ const App = () => {
                     <img src="./hero.png" alt="Hero Banner" />
                     <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
-                <p>Search</p>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
         </main>
     )
