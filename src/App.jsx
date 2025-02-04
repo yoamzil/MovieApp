e7b9e9d0f2682c613fe796f454053fc45509955a
Youness Amzil <amzilyouness@gmail.com>
2025-02-04 19:46:19 +0100

    Added APPWRITE to store user movie searches

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -45,6 +46,10 @@ const App = () => {
                 return;
             }
             setMovieList(data.results || []);
+
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
         }
         catch (error) {
             console.error(`Error fetching movies: ${error}`);
