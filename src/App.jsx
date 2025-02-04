bacfc4aeed061657a2e4a40883850cfd70cfc9cb
Youness Amzil <amzilyouness@gmail.com>
2025-02-04 18:03:05 +0100

    Search bar is working

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ const App = () => {
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
         setIsLoading(true);
         setErrorMessage('');
 
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query
+            ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
             const response = await fetch(endpoint, API_OPTIONS);
             if (!response.ok) {
@@ -50,8 +52,8 @@ const App = () => {
     }
 
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        fetchMovies(searchTerm);
+    }, [searchTerm]);
 
     return (
         <main>
